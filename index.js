f4d0577cb1a146a8c4407f2a4ba5241133802c9d
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-08-28 22:06:04 +0600

    coin reduce logic create and complete

--- index.js
+++ index.js
@@ -10,17 +10,35 @@ for (let allHeartIcon of heartIcon) {
     const increaseHeart = getElement("increase-heart").innerText;
     let totalHeart = Number(increaseHeart) + 1;
     getElement("increase-heart").innerText = Number(totalHeart);
-
-
-
-
-
-
   });
 }
 
+const callButton = document.getElementsByClassName("call-button");
 
-// after click call button 
+for (let allCalls of callButton) {
+  allCalls.addEventListener("click", function () {
+    const card = allCalls.closest(".cards");
 
-getElement("")
+    const cardName = card.querySelector(".card-name").innerText.trim();
+    const cardNum = card.querySelector(".card-number").innerText.trim();
 
+    alert(`Service Name: ${cardName}\nService Number: ${cardNum}`);
+
+    // coin reduce after each click
+
+    const coinButton = getElement("coin-button");
+
+    const totalCurrentCoin = Number(coinButton.innerText);
+
+    
+
+    if (totalCurrentCoin < 20) {
+      alert("A minimum of 20 coins is required to make a call.");
+      return;
+    }
+
+    const newAmountCoin = totalCurrentCoin - 20;
+    coinButton.innerText = newAmountCoin;
+    
+  })
+}
